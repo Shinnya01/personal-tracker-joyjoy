b52f5bec7f3ba50b7a436592013c3afbded3047f
Shinnya01 <carreon.carll@gmail.com>
2026-05-18 20:30:53 +0800

    Fix SPA refresh routing for PWA and Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
         globIgnores: ['**/icons/source.png'],
-        navigateFallback: '/offline.html',
+        navigateFallback: '/index.html',
       },
     }),
   ],
